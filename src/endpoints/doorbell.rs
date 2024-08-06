c1a18f52882a47e1b1a2be0493c73a5b30130817
FizzyApple12 <github@fizzyapple12.com>
2024-08-06 16:27:02 -0400

    Add keep alive ping to the doorbell as an attempt to fix WebSocket clients timing out

--- src/endpoints/doorbell.rs
+++ src/endpoints/doorbell.rs
@@ -25,11 +25,17 @@ struct DoorbellState {
     receiver: Receiver<bool>,
 }
 
+#[derive(Clone)]
+enum SocketSenderMessage {
+    Ring(bool),
+    PingPong(),
+}
+
 pub struct DoorbellModule {}
 
 impl EndpointModule for DoorbellModule {
     fn create_router() -> Router {
-        let (sender, receiver) = watch::channel(false);
+        let (sender, receiver) = watch::channel::<bool>(false);
 
         let state = DoorbellState { sender, receiver };
 
@@ -64,19 +70,58 @@ async fn handle_socket(state: DoorbellState, mut socket: WebSocket) {
 
     let mut ringing_reciever = state.sender.subscribe();
 
+    let (send_task_sender, mut send_task_receiver) =
+        watch::channel::<SocketSenderMessage>(SocketSenderMessage::PingPong());
+
+    let send_task_sender_2 = send_task_sender.clone();
+
     let mut send_task: JoinHandle<()> = tokio::spawn(async move {
         loop {
-            if (ringing_reciever.changed().await).is_ok()
-                && sender
-                    .send(Message::Text(format!(
-                        "{}",
-                        *ringing_reciever.borrow_and_update()
-                    )))
-                    .await
-                    .is_err()
-            {
-                return;
+            if (send_task_receiver.changed().await).is_ok() {
+                let next_message = send_task_receiver.borrow_and_update().clone();
+
+                match next_message {
+                    SocketSenderMessage::Ring(new_state) => {
+                        if sender
+                            .send(Message::Text(format!("{}", new_state)))
+                            .await
+                            .is_err()
+                        {
+                            return;
+                        }
+                    }
+                    SocketSenderMessage::PingPong() => {
+                        if sender
+                            .send(Message::Ping(vec![
+                                103, 111, 111, 100, 32, 109, 111, 114, 110, 105, 110, 103, 33, 33,
+                                33,
+                            ]))
+                            .await
+                            .is_err()
+                        {
+                            return;
+                        }
+                    }
+                }
             }
+        }
+    });
+
+    let mut ring_watcher_task: JoinHandle<()> = tokio::spawn(async move {
+        loop {
+            if (ringing_reciever.changed().await).is_ok() {
+                let _ = send_task_sender.send(SocketSenderMessage::Ring(
+                    *ringing_reciever.borrow_and_update(),
+                ));
+            }
+        }
+    });
+
+    let mut ping_pong_task: JoinHandle<()> = tokio::spawn(async move {
+        loop {
+            let _ = send_task_sender_2.send(SocketSenderMessage::PingPong());
+
+            tokio::time::sleep(std::time::Duration::from_millis(5000)).await;
         }
     });
 
@@ -104,10 +149,24 @@ async fn handle_socket(state: DoorbellState, mut socket: WebSocket) {
 
     tokio::select! {
         _rv_a = (&mut send_task) => {
+            ring_watcher_task.abort();
+            ping_pong_task.abort();
             recv_task.abort();
         },
         _rv_b = (&mut recv_task) => {
             send_task.abort();
+            ring_watcher_task.abort();
+            ping_pong_task.abort();
+        }
+        _rv_c = (&mut ring_watcher_task) => {
+            send_task.abort();
+            ping_pong_task.abort();
+            recv_task.abort();
+        }
+        _rv_d = (&mut ping_pong_task) => {
+            send_task.abort();
+            ring_watcher_task.abort();
+            recv_task.abort();
         }
     }
 }
