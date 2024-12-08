efe2469206165177407a75e545512df2daa86945
FizzyApple12 <github@fizzyapple12.com>
2024-12-08 22:10:15 +0000

    updated nix package metadata

--- package.nix
+++ package.nix
@@ -2,18 +2,20 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "purdue-hackers-api";
-  version = "0.1.0";
+  version = "0.2.0";
+
+  meta.mainProgram = "backend";
 
   src = fetchFromGitHub {
     owner = "purduehackers";
     repo = "api";
-    rev = "88a813f92f6ea3d65ef0b454ac19b5564af185fb";
-    hash = "";
+    rev = "e4fd55b2cbc7caa714a2e5e6954f7d17c0eb5956";
+    hash = "sha256-oYuX/X95WQh5mQJDoh2W7wzzqX7nscdS/JrRVornaRA=";
   };
 
   nativeBuildInputs = [ pkg-config ];
   buildInputs = [ openssl ];
 
-  cargoHash = "";
+  cargoHash = "sha256-aWZjA+/FFidG15W4n85NZNrTyFWWSD0dkJC36SJHZ6I=";
   cargoDepsName = pname;
 }
